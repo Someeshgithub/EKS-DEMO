2940434a03cfb589481c399f6d963ceb692fa136
someeshwar <someesh1292@gmail.com>
2026-06-28 21:32:28 +0530

    test

--- lib/eks-stack.ts
+++ lib/eks-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as eks from 'aws-cdk-lib/aws-eks';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { KubectlV31Layer } from '@aws-cdk/lambda-layer-kubectl-v31';
 
@@ -14,9 +15,13 @@ export class EksStack extends cdk.Stack {
       version: eks.KubernetesVersion.V1_31,
       vpc: vpc,
       defaultCapacity: 0,
-
-      // Add this
       kubectlLayer: new KubectlV31Layer(this, 'KubectlLayer'),
+    });
+
+    cluster.awsAuth.addUserMapping(new iam.User(this, 'EksAdminUser', {
+      userName: 'Tharini'
+    }), {
+      groups: ['system:masters']
     });
   }
 }
