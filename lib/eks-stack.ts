545c5e0fc1d183dbf22b51b0cef14e9424e5e102
someeshwar <someesh1292@gmail.com>
2026-06-28 20:22:00 +0530

    updated manifest

--- lib/eks-stack.ts
+++ lib/eks-stack.ts
@@ -1,16 +1,23 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as eks from 'aws-cdk-lib/aws-eks';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-export class EksCdkDemoStack extends cdk.Stack {
+export class EksStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EksCdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 2
+    });
+    const cluster = new eks.Cluster(this, 'EksCluster', {
+      version: eks.KubernetesVersion.V1_31,
+      vpc,
+      defaultCapacity: 0
+    });
+    cluster.addNodegroupCapacity('NodeGroup', {
+      desiredSize: 2,
+      minSize: 1,
+      maxSize: 3,
+      instanceTypes: [new ec2.InstanceType('t3.medium')]
+    });
   }
 }
